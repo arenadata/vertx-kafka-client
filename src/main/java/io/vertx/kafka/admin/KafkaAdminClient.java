63eb7774a68be0165d5fc7cacd5604a2647ca365
kuhella <ke@arenadata.io>
2021-10-24 23:29:24 +0300

    ADSCC-161 Added electLeaders to KafkaAdminClient

--- src/main/java/io/vertx/kafka/admin/KafkaAdminClient.java
+++ src/main/java/io/vertx/kafka/admin/KafkaAdminClient.java
@@ -17,22 +17,19 @@
 package io.vertx.kafka.admin;
 
 import io.vertx.codegen.annotations.GenIgnore;
-import io.vertx.core.Future;
-import io.vertx.kafka.client.common.ConfigResource;
-import org.apache.kafka.clients.admin.AdminClient;
-
 import io.vertx.codegen.annotations.VertxGen;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.kafka.admin.impl.KafkaAdminClientImpl;
+import io.vertx.kafka.client.common.ConfigResource;
+import io.vertx.kafka.client.common.ElectionType;
 import io.vertx.kafka.client.common.TopicPartition;
 import io.vertx.kafka.client.consumer.OffsetAndMetadata;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
+import org.apache.kafka.clients.admin.AdminClient;
+
+import java.util.*;
 
 /**
  * Vert.x Kafka Admin client implementation
@@ -300,6 +297,12 @@ public interface KafkaAdminClient {
    */
   @GenIgnore
   Future<Map<TopicPartition, ListOffsetsResultInfo>> listOffsets(Map<TopicPartition, OffsetSpec> topicPartitionOffsets);
+
+  @GenIgnore
+  void electLeaders(ElectionType electionType, Set<TopicPartition> partitions, Handler<AsyncResult<Void>> completionHandler);
+
+  @GenIgnore
+  Future<Void> electLeaders(ElectionType electionType, Set<TopicPartition> partitions);
 
   /**
    * Close the admin client
